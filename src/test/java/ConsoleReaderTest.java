f20d3c1a9709f0ff4a5edbf694db7ed06d3bdb96
chirkov86 <p3aNepav!>
2018-11-24 21:34:44 +0300

    Comment empty test class

--- src/test/java/ConsoleReaderTest.java
+++ src/test/java/ConsoleReaderTest.java
@@ -1,22 +1,22 @@
-import com.achirkov.codingpuzzle.game.GameState;
-import com.achirkov.codingpuzzle.io.ConsoleReader;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-
-@RunWith(MockitoJUnitRunner.class)
-public class ConsoleReaderTest {
-
-    @InjectMocks
-    private ConsoleReader consoleReader = new ConsoleReader();
-
-    @Mock
-    private GameState GameState;
-
-//    @Before
-//    void setup() {
-//        MockitoAnnotations.initMocks(this);
-//    }
-
-}
+//import com.achirkov.codingpuzzle.game.GameState;
+//import com.achirkov.codingpuzzle.io.ConsoleReader;
+//import org.junit.runner.RunWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.junit.MockitoJUnitRunner;
+//
+//@RunWith(MockitoJUnitRunner.class)
+//public class ConsoleReaderTest {
+//
+//    @InjectMocks
+//    private ConsoleReader consoleReader = new ConsoleReader();
+//
+//    @Mock
+//    private GameState GameState;
+//
+////    @Before
+////    void setup() {
+////        MockitoAnnotations.initMocks(this);
+////    }
+//
+//}
