419ab27d06a1774a9abedf1ef9055a55818d50a2
chirkov86 <andrey.chirkov@zedconnect.com>
2018-11-25 14:11:40 +0300

    Added test for GameContextManager

--- src/test/java/com/achirkov/codingpuzzle/game/GameContextManagerTest.java
+++ src/test/java/com/achirkov/codingpuzzle/game/GameContextManagerTest.java
@@ -1,20 +1,44 @@
 package com.achirkov.codingpuzzle.game;
 
+import com.achirkov.codingpuzzle.creatures.Knight;
+import com.achirkov.codingpuzzle.positioning.Position;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.File;
 
 public class GameContextManagerTest {
 
+    private GameContextManager gameContextManager;
+
+    @Before
+    public void setUp() throws Exception {
+        gameContextManager = new GameContextManager();
+        gameContextManager.setPlayer(new Knight("Player1", Position.initial()));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        File file = new File("savegames/savegame");
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+
     @Test
     public void save() {
+        gameContextManager.save();
     }
 
     @Test
     public void load() {
+        gameContextManager.save();
+        gameContextManager.load();
     }
 
     @Test
     public void init() {
+        gameContextManager.init();
     }
 }
